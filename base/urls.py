e5b5da7d80979812cdfe4683c3b3cb428cd3107b
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-15 10:57:01 +0300

    ft:included the profile urls

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 
 urlpatterns=[
-    path('',views.HomeView.as_view(),name='home'),
+    path('',views.home,name='home'),
+    path('profile/<str:username>/settings',views.edit_profile,name='settings'),
+    path('project/<str:post>/',views.project,name='project'),
+    path('profile/<username>/', views.profile, name='profile'),
+
     path('login/',views.LoginInterfaceView.as_view(),name='login'),
     path('logout/',views.logoutUser,name='logout'),
     path('signup/',views.SignupView.as_view(),name='signup'),
