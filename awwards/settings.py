6da10745401ca629a4dec90e365f4b1b3c88f2da
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-15 11:52:28 +0300

    ft:deployment to heroku

--- awwards/settings.py
+++ awwards/settings.py
@@ -15,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g-u(!*lds!p@n13w$3-t1j&t-2e&599bwx(dcr(6w8t4u!bg&^'
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 MODE='prod'
 
 ALLOWED_HOSTS = ['.localhost', '.herokuapp.com', '127.0.0.1']
